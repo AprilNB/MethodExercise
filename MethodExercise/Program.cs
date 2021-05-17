9b6e3c4ca9bd73972f7bd81db6048031b639d547
April Bullard <aprilnb86@gmail.com>
2021-05-17 14:48:43 -0500

    finished profile configuration

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -18,6 +18,13 @@ namespace MethodExercise
             Console.WriteLine("Awesome! What is your favorite band?");
             var band = Console.ReadLine();
 
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
         }
     }
 }
